d8567e7fbe8507e3d071b26787a20bd4bdf31681
Andres Vertel <avertel@gmail.com>
2024-05-20 12:02:29 -0500

    first commit

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from users import views
+
+urlpatterns = [
+    path("create/", views.CreateUserView.as_view()),
+    path("token/", views.CreateTokenView.as_view()),
+    path("user/", views.RetreiveUpdateUserView.as_view()),
+]
---
+++ users/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken 
+from users.models import User
+from users.serializers import UserSerializer, AuthTokenSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+class RetreiveUpdateUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
